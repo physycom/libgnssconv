1734538cf44c6fb205f45cd337ad2a9a608bf505
Alessandro Fabbri <rommacc@yahoo.it>
2017-03-27 11:48:41 +0100

    add a comment

--- libgnssconv.hpp
+++ libgnssconv.hpp
@@ -17,7 +17,7 @@ You should have received a copy of the GNU General Public License
 along with gnss_conversion. If not, see <http://www.gnu.org/licenses/>.
 ***************************************************************************/
 
-#ifndef _GNU_SOURCE
+#ifndef _GNU_SOURCE      // enable M_PI in gcc
 #define _GNU_SOURCE
 #endif
 
